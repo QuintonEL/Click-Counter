c07762c94f7df1dee1b30547f791a877750eea57
QuintonEL <quintonlaborde@gmail.com>
2020-04-16 13:23:14 -0600

    finished button functionalitty and added some styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './App.css';
+
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
+      <div className='webdesigntuts-workshop'>
         <Counter />
       </div>
     )
@@ -11,8 +13,18 @@ class App extends React.Component {
 }
 
 class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  handleClick = () => {
+    this.setState((prevState, { count }) => ({
+      count: prevState.count + 1
+    }));
+  };
+
   render() {
-    return <button>0</button>;
+  return <button onClick={this.handleClick}>{this.state.count}</button>;
   }
 }
 
